a3c630542ab1a00e4e469601695cb98dd805804b
Arturo Reyna <a.reyna@uniandes.edu.co>
2022-11-03 21:40:27 -0600

    Editar servicio

--- src/app/vehiculo/vehiculo.service.ts
+++ src/app/vehiculo/vehiculo.service.ts
@@ -1,10 +1,18 @@
 import { Injectable } from '@angular/core';
+import { environment } from '../../environments/environment';
+import { HttpClient } from  '@angular/common/http'
+import { Observable } from 'rxjs';
+import { Vehiculo } from './vehiculo';
 
 @Injectable({
   providedIn: 'root'
 })
 export class VehiculoService {
 
-constructor() { }
+  constructor( private http: HttpClient) { }
+
+  getVehiculos() :Observable<Vehiculo[]> {
+    return  this.http.get<Vehiculo[]>(environment.baseUrl);
+  }
 
 }
