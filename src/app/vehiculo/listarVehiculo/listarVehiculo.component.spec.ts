a2c36c47d53db20b873bdd4aba87c83b81bb6514
Arturo Reyna <a.reyna@uniandes.edu.co>
2022-11-03 23:45:31 -0600

    Added test

--- src/app/vehiculo/listarVehiculo/listarVehiculo.component.spec.ts
+++ src/app/vehiculo/listarVehiculo/listarVehiculo.component.spec.ts
@@ -2,6 +2,7 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { By } from '@angular/platform-browser';
 import { DebugElement } from '@angular/core';
+import { Vehiculo } from '../vehiculo';
 
 import { ListarVehiculoComponent } from './listarVehiculo.component';
 
@@ -25,4 +26,18 @@ describe('ListarVehiculoComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('component should render same number of rows as list plus header', async(() => {
+    for(let i = 0; i < 3; i++) {
+      const vehiculo = new Vehiculo();
+      vehiculo.id = i;
+
+      component.vehiculos.push(vehiculo);
+    };
+    fixture.detectChanges();
+    const compiled = fixture.debugElement.nativeElement;
+    let rows = compiled.querySelectorAll('tr')
+    expect(rows.length).toEqual(4);
+  }));
+
 });
